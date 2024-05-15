95e9a2ccc8c92fca7ad37acadd9b340052e033c9
Veselov Max <veselov.maksim@lit1533.ru>
2024-05-15 23:59:24 +0300

    created 44 aand copied from 43

---
+++ mvvm44/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace mvvm44.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
---
+++ mvvm44/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace mvvm44.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
